d22e43e28127bc7678dad2108e92067e464c38dc
AlexandruBurlica <152985864+burlica1234@users.noreply.github.com>
2025-10-17 11:48:26 +0300

    Delete Domain directory

--- Domain/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp2.Domain;
-
-public class Book
-{
-    public int Id { get; set; }
-    public String Title { get; set; } = String.Empty;
-    public String Author  { get; set; } = String.Empty;
-    public int Year { get; set; }
-}
