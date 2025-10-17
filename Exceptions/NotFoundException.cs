8efc1bf3855a84bbd22ac813e0863e02913ca6c0
AlexandruBurlica <152985864+burlica1234@users.noreply.github.com>
2025-10-17 11:48:53 +0300

    Delete Exceptions directory

--- Exceptions/NotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsoleApp2.Exceptions;
-
-public class NotFoundException : Exception
-{
-    public NotFoundException(string resource, object key)
-        : base($"{resource} with key '{key}' was not found.")
-    {
-    }
-}
